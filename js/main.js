a28d5a68dd1f0eb200a1e44345d838700a4f47bd
hulk91 <hulk.91@list.ru>
2016-07-14 05:44:13 +0800

    FIXES12

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function parallax() {
             // Значение yPos отрицательное, так как прокручивание осуществляется вверх!
             var yPos = -($window.scrollTop() / $bgobj.data('speed'));
             // Размещаем все вместе в конечной точке
-            var coords = '50% '+ yPos + 'px';
+            var coords = '50% '+ yPos +'px';
             // Смещаем фон
             $bgobj.css({ backgroundPosition: coords });
         });
@@ -83,6 +83,21 @@ function tooltip(){
 //         dynamicHandle: 1
 //     });
 // }
+
+
+
+
+
+function buttonUp(){
+    var inputVal = $('.searchbox-input').val();
+    inputVal = $.trim(inputVal).length;
+    if( inputVal !== 0){
+        $('.searchbox-icon').css('display','none');
+    } else {
+        $('.searchbox-input').val('');
+        $('.searchbox-icon').css('display','block');
+    }
+}
 
 
 
@@ -235,6 +250,34 @@ function tooltip(){
         //     return false;
         // });
 
+
+        var submitIcon = $('.searchbox-icon');
+        var inputBox = $('.searchbox-input');
+        var searchBox = $('.searchbox');
+        var isOpen = false;
+        submitIcon.click(function(){
+            if(isOpen == false){
+                searchBox.addClass('searchbox-open');
+                inputBox.focus();
+                isOpen = true;
+            } else {
+                searchBox.removeClass('searchbox-open');
+                inputBox.focusout();
+                isOpen = false;
+            }
+        });
+        submitIcon.mouseup(function(){
+            return false;
+        });
+        searchBox.mouseup(function(){
+            return false;
+        });
+        $(document).mouseup(function(){
+            if(isOpen == true){
+                $('.searchbox-icon').css('display','block');
+                submitIcon.click();
+            }
+        });
 
 
 
