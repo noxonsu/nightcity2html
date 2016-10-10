1a43fe129f38e2948402567268700f4e5fdaba98
nthgness <zalunin.alexey@gmail.com>
2016-10-10 23:16:40 +0300

    test main_js() #11

--- js/main.js
+++ js/main.js
@@ -180,7 +180,7 @@ google.maps.event.addDomListener(window, 'load', init_map);
     $(document).ready(function () {
         'use strict';
         $('html').removeClass('no-js');
-        $('.chosen-single').addClass('noajax');
+        //$('.chosen-single').addClass('noajax');
 
 
         //****************************** Ion range slider plugin activate ******************************//
